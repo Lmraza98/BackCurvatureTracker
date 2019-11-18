898dbde852c58a61cf16a0c898a534a0860cf1a6
Lucas Raza <razal@wit.edu>
2019-11-18 10:41:09 -0500

    ** Sign - Up and Log In Screens fleshed out, not connected to Database yet

---
+++ app/src/main/java/mobilehealth/wit/myapplication/LoginActivity.java
@@ -0,0 +1,25 @@
+package mobilehealth.wit.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView signUp_text = findViewById(R.id.signUp_text);
+        signUp_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/mobilehealth/wit/myapplication/RegistrationActivity.java
@@ -0,0 +1,25 @@
+package mobilehealth.wit.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        TextView signIn_text = findViewById(R.id.signIn_text);
+        signIn_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+    }
+}
