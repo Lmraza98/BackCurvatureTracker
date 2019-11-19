fc0bc5927ea0c2f79dc0fd88107baab88318b0a4
Lucas Raza <razal@wit.edu>
2019-11-19 15:19:20 -0500

    ** Bluetooth fragment shows available bluetooth devices

--- app/src/main/java/mobilehealth/wit/myapplication/setup/BluetoothFragment.java
+++ app/src/main/java/mobilehealth/wit/myapplication/setup/BluetoothFragment.java
@@ -1,11 +1,21 @@
 package mobilehealth.wit.myapplication.setup;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import androidx.fragment.app.Fragment;
 import mobilehealth.wit.myapplication.R;
@@ -29,6 +39,9 @@ public class BluetoothFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private ListView listView;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
+    private BluetoothAdapter mBluetoothAdapter;
 
     public BluetoothFragment() {
         // Required empty public constructor
@@ -59,11 +72,22 @@ public class BluetoothFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        listView = (ListView) getView().findViewById(R.id.listView);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter.startDiscovery();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        getActivity().registerReceiver(mReceiver, filter);
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bluetooth, container, false);
     }
@@ -74,6 +98,20 @@ public class BluetoothFragment extends Fragment {
             mListener.onFragmentInteraction(uri);
         }
     }
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getName() + "\n" + device.getAddress());
+                Log.i("BT", device.getName() + "\n" + device.getAddress());
+                listView.setAdapter(new ArrayAdapter<String>(context,
+                        android.R.layout.simple_list_item_1, mDeviceList));
+            }
+        }
+    };
 
     @Override
     public void onAttach(Context context) {
