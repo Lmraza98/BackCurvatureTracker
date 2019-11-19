f1bb5242ee113495da60f0f1b42e30aaa24a4fa3
Lucas Raza <razal@wit.edu>
2019-11-19 13:24:10 -0500

    ** Added navigation bar to MainActivity, fragmented with - dashboard fragment (will have graphs on it to display information from arduino) - home fragment (displays information to have better posture) - notifications fragment( allows the user to change the settings of the app)

--- app/src/main/java/mobilehealth/wit/myapplication/MainActivity.java
+++ app/src/main/java/mobilehealth/wit/myapplication/MainActivity.java
@@ -1,11 +1,14 @@
 package mobilehealth.wit.myapplication;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        GoogleSignInAccount account = getIntent().getParcelableExtra("GOOGLE");
-
-       TextView text = findViewById(R.id.name);
-       text.setText(account.getDisplayName());
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
     }
+
 }
