e7a803ac8c99459c8d88fbab5e20b09bad4aec2c
Lucas Raza <razal@wit.edu>
2019-11-19 11:19:52 -0500

    ** Can Log-In to Google Account

--- app/src/main/java/mobilehealth/wit/myapplication/LoginActivity.java
+++ app/src/main/java/mobilehealth/wit/myapplication/LoginActivity.java
@@ -2,16 +2,38 @@ package mobilehealth.wit.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class LoginActivity extends AppCompatActivity {
+   private FirebaseAuth auth;
+   private int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+       // auth = FirebaseAuth.getInstance();
+
 
         TextView signUp_text = findViewById(R.id.signUp_text);
         signUp_text.setOnClickListener(new View.OnClickListener() {
@@ -21,5 +43,54 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+
+         Button login = findViewById(R.id.user_login);
+         login.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v)
+             {
+                 switch (v.getId())
+                 {
+                     case R.id.user_login:
+                         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                         startActivityForResult(signInIntent, RC_SIGN_IN);
+                         break;
+                 }
+             }
+
+         });
+
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Result returned from launching the Intent from GoogleSignInClient.getSignInintent(...);
+        if (requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask)
+    {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+        } catch(ApiException e )
+        {
+            Log.w("TAG", "signInResult:Failed code=" + e.getStatusCode());
+        }
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        //UPDATE UI
+        //updateUI(account);
+    }
+
 }
--- app/src/main/java/mobilehealth/wit/myapplication/MainActivity.java
+++ app/src/main/java/mobilehealth/wit/myapplication/MainActivity.java
@@ -1,14 +1,14 @@
 package mobilehealth.wit.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
